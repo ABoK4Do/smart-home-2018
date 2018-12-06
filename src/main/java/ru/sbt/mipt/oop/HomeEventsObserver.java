509c6d33c1e4411504daecdb40ff30833ea646f7
ABoK4Do <alesha148@gmail.com>
2018-12-07 02:19:02 +0300

    Добавил шаблон Observer

---
+++ src/main/java/ru/sbt/mipt/oop/HomeEventsObserver.java
@@ -0,0 +1,29 @@
+package ru.sbt.mipt.oop;
+
+import java.util.ArrayList;
+
+public class HomeEventsObserver implements Observer {
+    ArrayList<EventProcessor> subscribers = new ArrayList<>();
+    SmartHome smartHome;
+
+    HomeEventsObserver(SmartHome smartHome) {
+        this.smartHome = smartHome;
+    }
+
+    @Override
+    public void subscribe(EventProcessor processor) {
+        subscribers.add(processor);
+    }
+
+    @Override
+    public void unsubscribe(EventProcessor processor) {
+        subscribers.remove(processor);
+    }
+
+    @Override
+    public void notifySubscribers(SensorEvent event) {
+        for (EventProcessor subscriber : subscribers) {
+            subscriber.processEvent(smartHome, event);
+        }
+    }
+}
