4d9724c41cc98e385c61c897a731610b2e527ae9
ABoK4Do <alesha148@gmail.com>
2018-12-07 03:02:10 +0300

    Выполняет последовательно набор event'ов из EventProvider

--- src/main/java/ru/sbt/mipt/oop/EventManager.java
+++ src/main/java/ru/sbt/mipt/oop/EventManager.java
@@ -1,5 +1,28 @@
 package ru.sbt.mipt.oop;
 
-public interface EventManager {
-    void runEventsCycle(SmartHome smartHome);
+import java.util.Collection;
+
+public class EventManager {
+    final private SensorEventProvider sensorEventProvider;
+
+
+    EventManager(SensorEventProvider sensorEventProvider) {
+        this.sensorEventProvider = sensorEventProvider;
+    }
+    public void runEventsCycle(SmartHome smartHome) {
+        HomeEventsObserver homeEventsObserver = new HomeEventsObserver(smartHome);
+        // начинаем цикл обработки событий
+        SensorEvent event = sensorEventProvider.getNextSensorEvent();
+        Collection<EventProcessor> eventProcessors = ConfigProcessors.configureEventProcessors();
+        //Подписываем всех процессоров на наблюдателя
+        for (EventProcessor processor : eventProcessors) {
+            homeEventsObserver.subscribe(processor);
+        }
+
+        while (event != null) {
+            System.out.println("Got event: " + event);
+            homeEventsObserver.notifySubscribers(event);
+            event = sensorEventProvider.getNextSensorEvent();
+        }
+    }
 }
